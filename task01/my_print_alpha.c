28c1e0f6416200306de2de580813b3a101fa8028
Lopepi <lopepi@outlook.fr>
2025-09-21 19:21:15 +0200

    Task01

---
+++ task01/my_print_alpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "my_print_alpha.h"
+
+int my_print_alpha(void)
+{
+	char c = 'a';
+
+	for (c ='a'; c <= 'z'; c++)
+	{
+		printf("%c", c);
+	}
+
+	printf("\n");
+
+	return 0;
+}
