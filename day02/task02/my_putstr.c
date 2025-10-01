1ccdd689fc359f8460a80f74b94c7980e3f396e0
Lopepi <lopepi@outlook.fr>
2025-10-01 22:24:57 +0200

    day02-task03

--- day02/task02/my_putstr.c
+++ day02/task02/my_putstr.c
@@ -1,11 +1,10 @@
-#include "unistd.h"
+#include <unistd.h>
 #include "my_putstr.h"
 
 //Write a function that displays, one-by-one, the characters of a string. The address of the string’s first character will be found in the pointer passed as a parameter to the function, which must be prototyped as follows: int my_putstr ( char const * str ) ;
 
 int my_putstr(char const *str)
 {
-
 	int i = 0;
 
 	while(str[i] != '\0')
@@ -18,5 +17,4 @@ int my_putstr(char const *str)
 	write(1, "\n", 1);
 
 	return 0;
-
 }
---
+++ day02/task03/my_strlen.h
@@ -0,0 +1,6 @@
+#ifndef MY_STRLEN
+#define MY_STRLEN
+
+int my_strlen(char const *str);
+
+#endif
