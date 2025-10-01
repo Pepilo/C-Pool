7b6b8c79235e7f1461ae09de0e980269f5168e63
Lopepi <lopepi@outlook.fr>
2025-10-01 19:07:08 +0200

    day02 - taskO1

---
+++ day02/task01/my_swap.c
@@ -0,0 +1,8 @@
+#include "my_swap.h"
+
+void my_swap(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
---
+++ day02/task01/my_swap.h
@@ -0,0 +1,6 @@
+#ifndef MY_SWAP
+#define MY_SWAP
+
+void my_swap(int *a, int *b);
+
+#endif
