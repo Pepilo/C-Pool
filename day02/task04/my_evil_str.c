b8bf51fe04a85e76c0ad53c3637e23ba1ed2f165
Lopepi <lopepi@outlook.fr>
2025-10-02 12:19:48 +0200

    day02-task04

---
+++ day02/task04/my_evil_str.c
@@ -0,0 +1,30 @@
+#include "my_evil_str.h"
+
+//The goal of this task is to swap each of the string’s characters, two by two. In other words, you will swap the first letter with the last one, the second with the second-to-last and so on. The function should return a pointer to the first character of the reversed string: char * my_evil_str ( char * str ) ;
+
+char *my_evil_str(char *str)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	while(str[i] != '\0')
+	{
+		i++;
+	}
+
+	char temp_str[i];
+
+	for(j = 0; j < i; j++)
+	{
+		temp_str[j] = str[j];
+	}
+
+	while(k < i)
+	{
+		str[k] = temp_str[i-(k+1)];
+		k++;
+	}
+
+	return str;
+}
---
+++ day02/task04/my_evil_str.h
@@ -0,0 +1,6 @@
+#ifndef MY_EVIL_STR
+#define MY_EVIL_STR
+
+char *my_evil_str(char *str);
+
+#endif
