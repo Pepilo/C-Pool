44fce76b9d339c4f2b8c2ec1521a03c522741fee
Lopepi <lopepi@outlook.fr>
2025-10-03 18:55:50 +0200

    day03-task03

---
+++ day03/task03/my_compute_power_it.c
@@ -0,0 +1,21 @@
+#include "my_compute_power_it.h"
+
+//Write an iterative function that returns the first argument raised to the power p, where p is the second argument. It must be prototyped the following way: int my_compute_power_it (int nb , int p ) ;
+
+int my_compute_power_it(int nb, int p)
+{
+	if(p < 0)
+	{
+		return 0;
+	}
+
+	int result = 1;
+
+	while(p > 0)
+	{
+		result *= nb;
+		p --;
+	}
+
+	return result;
+}
---
+++ day03/task03/my_compute_power_it.h
@@ -0,0 +1,6 @@
+#ifndef MY_COMPUTE_POWER_IT
+#define MY_COMPUTE_POWER_IT
+
+int my_compute_power_it(int nb, int p);
+
+#endif
