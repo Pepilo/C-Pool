04aab57efa2ec6c9231548858fcdb8a8fd49382b
Lopepi <lopepi@outlook.fr>
2025-10-03 18:29:53 +0200

    day03-task01

---
+++ day03/task01/my_compute_factorial_it.c
@@ -0,0 +1,22 @@
+#include "my_include_factorial_it.h"
+
+//Write an iterative function that returns the factorial of the number given as a parameter. It must be prototyped the following way: int my_compute_factorial_it (int nb ) ;
+
+int my_compute_factorial_it(int nb)
+{
+	int i = 0;
+
+	if(nb < 0)
+	{
+		return 0;
+	}
+
+	int result = 1;
+
+	for(i = nb; i >= 1; i--)
+	{
+		result *= i;
+	}
+
+	return result;
+}
---
+++ day03/task01/my_compute_factorial_it.h
@@ -0,0 +1,6 @@
+#ifndef MY_COMPUTE_FACTORIAL_IT
+#define MY_COMPUTE_FACTORIAL_IT
+
+int my_compute_factorial_it(int nb);
+
+#endif
