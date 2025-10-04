422b3e78878adbc66bf5cbed8ab436e82cb4f7ea
Lopepi <lopepi@outlook.fr>
2025-10-04 18:57:02 +0200

    day03-task06

---
+++ day03/task06/my_is_prime.c
@@ -0,0 +1,25 @@
+#include "my_is_prime.h"
+
+//Write a function that returns 1 if the number is prime and 0 if not. It must be prototyped the following way: int my_is_prime (int nb ) ;
+
+int my_is_prime(int nb)
+{
+	int i = 1;
+
+	if(nb == 0 || nb == 1)
+	{
+		return 0;
+	}
+
+	while(i < nb)
+	{
+		i++;
+
+		if(nb % i == 0)
+		{
+			return 0;
+		}
+	}
+
+	return 1;
+}
---
+++ day03/task06/my_is_prime.h
@@ -0,0 +1,6 @@
+#ifndef MY_IS_PRIME
+#define MY_IS_PRIME
+
+int my_is_prime(int nb);
+
+#endif
