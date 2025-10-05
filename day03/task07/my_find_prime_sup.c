0a105558fcba41a88d6440f8ca7f1f6d8f209d40
Lopepi <lopepi@outlook.fr>
2025-10-05 13:52:58 +0200

    day03-task07

---
+++ day03/task07/my_find_prime_sup.c
@@ -0,0 +1,25 @@
+#include "my_find_prime_sup.h"
+
+//Write a function that returns the smallest prime number that is greater than, or equal to, the number given as a parameter. It must be prototyped the following way: int my_find_prime_sup (int nb ) ;
+
+int my_find_prime_sup(int nb)
+{
+	if(nb <= 1)
+	{
+		return my_find_prime_sup(nb + 1);
+	}
+
+	int i = 2;
+
+	while(i < nb)
+	{
+		if(nb % i == 0)
+		{
+			return my_find_prime_sup(nb + 1);
+		}
+
+		i++;
+	}
+
+	return nb;
+}
