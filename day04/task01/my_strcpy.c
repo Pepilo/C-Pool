8e08bb61448cd6fd616da6b402b8196c6a772157
Lopepi <lopepi@outlook.fr>
2025-10-07 13:14:43 +0200

    day04-task01

---
+++ day04/task01/my_strcpy.c
@@ -0,0 +1,18 @@
+#include "my_strcpy.h"
+
+//Write a function that copies a string into another. The destination string will already have enough memory to copy the source string. It must be prototyped the following way: char * my_strcpy ( char * dest , char const * src ) ;
+
+char *my_strcpy(char *dest, char const *src)
+{
+	int i = 0;
+
+	while(src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return dest;
+}
---
+++ day04/task01/my_strcpy.h
@@ -0,0 +1,6 @@
+#ifndef MY_STRCPY
+#define MY_STRCPY
+
+char *my_strcpy(char *dest, char const *src);
+
+#endif
