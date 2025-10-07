efe1e709c26dcee88ebbc5d5adfe08b6c0444b2d
Lopepi <lopepi@outlook.fr>
2025-10-07 13:29:24 +0200

    day04-task02

---
+++ day04/task02/my_strncpy.c
@@ -0,0 +1,23 @@
+#include "my_strncpy.h"
+
+//Write a function that copies n characters from a string into another. The destination string will already have enough memory to contain n characters. It must be prototyped the following way: char * my_strncpy ( char * dest , char const * src , int n ) ;
+
+char *my_strncpy(char *dest, char const *src, int n)
+{
+	int i = 0;
+
+	while(i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+
+		i++;
+	}
+
+	while(i < n)
+	{
+		dest[i] = '\0';
+		i++
+	}
+
+	return dest;
+}
---
+++ day04/task02/my_strncpy.h
@@ -0,0 +1,6 @@
+#ifndef MY_STRNCPY
+#define MY_STRNCPY
+
+char *my_strncpy(char *dest, char const *src, int n);
+
+#endif
