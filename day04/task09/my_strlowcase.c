07c4749dcc5f151fba0bb02c8efe4645e185c729
Lopepi <lopepi@outlook.fr>
2025-10-08 18:29:46 +0200

    day04-task09

---
+++ day04/task09/my_strlowcase.c
@@ -0,0 +1,20 @@
+#include "my_strlowcase.h"
+
+//Write a function that puts every letter of every word in it in lowercase. It must be prototyped the following way: char * my_strlowcase ( char * str ) ;
+
+char *my_strlowcase(char *str)
+{
+	int i = 0;
+
+	while(str[i] != '\0')
+	{
+		if(str[i] >= 65 && str[i] <= 90)
+		{
+			str[i] = str[i] + 32;
+		}
+
+		i++;
+	}
+
+	return str;
+}
---
+++ day04/task09/my_strlowcase.h
@@ -0,0 +1,6 @@
+#ifndef MY_STRLOWCASE
+#define MY_STRLOWCASE
+
+char *my_strlowcase(char *str);
+
+#endif
