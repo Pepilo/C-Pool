a1ca6e81c95735d67033097a7783d6937e5e6c3b
Lopepi <lopepi@outlook.fr>
2025-10-08 17:10:01 +0200

    day04-task05

---
+++ day04/task05/my_strstr.c
@@ -0,0 +1,33 @@
+#include "my_strstr.h"
+
+//Reproduce the behavior of the strstr function. Your function must be prototyped the following way: char * my_strstr ( char * str , char const * to_find ) ;
+
+char *my_strstr(char *str, char const *to_find)
+{
+	int i =0;
+	int j = 0;
+
+	if(to_find[0] == '\0')
+	{
+		return str;
+	}
+
+	while(str[i] != '\0')
+	{
+		j = 0;
+
+		while(str[i + j] == to_find[j] && to_find[j] != '\0')
+		{
+			j++;
+		}
+
+		if(to_find[j] == '\0')
+		{
+			return &str[i];
+		}
+
+		i++;
+	}
+
+	return 0;
+}
---
+++ day04/task05/my_strstr.h
@@ -0,0 +1,6 @@
+#ifndef MY_STRSTR
+#define MY_STRSTR
+
+char *my_strstr(char *str, char const *to_find);
+
+#endif
