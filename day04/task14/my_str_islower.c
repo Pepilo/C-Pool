ea65156bac791d5d4f193c35b4121251ec0fa8d0
Lopepi <lopepi@outlook.fr>
2025-10-11 12:51:39 +0200

    day04-task14

---
+++ day04/task14/my_str_islower.c
@@ -0,0 +1,20 @@
+#include "my_str_islower.h"
+
+//Write a function that returns 1 if the string passed as parameter only contains lowercase alphabetical char␂acters and 0 otherwise. It must be prototyped the following way: int my_str_islower ( char const * str ) ;
+
+int my_str_islower(char const *str)
+{
+	int i = 0;
+
+	while(str[i] != '\0')
+	{
+		if(!(str[i] >= 97 && str[i] <= 122))
+		{
+			return 0;
+		}
+
+		i++;
+	}
+
+	return 1;
+}
---
+++ day04/task14/my_str_islower.h
@@ -0,0 +1,6 @@
+#ifndef MY_STR_ISLOWER
+#define MY_STR_ISLOWER
+
+int my_str_islower(char const *str);
+
+#endif
