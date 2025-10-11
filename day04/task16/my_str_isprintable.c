5117c21d5c08460cc132fd8cb64040194875daad
Lopepi <lopepi@outlook.fr>
2025-10-11 12:59:48 +0200

    day04-task16

---
+++ day04/task16/my_str_isprintable.c
@@ -0,0 +1,20 @@
+#include "my_str_isprintable.h"
+
+//Write a function that returns 1 if the string passed as parameter only contains printable characters and 0 otherwise. It must be prototyped the following way: int my_str_isprintable ( char const * str ) ;
+
+int my_str_isprintable(char const *str)
+{
+	int i = 0;
+
+	while(str[i] != '\0')
+	{
+		if(!(str[i] >= 32 && str[i] <= 126))
+		{
+			return 0;
+		}
+
+		i++;
+	}
+
+	return 1;
+}
---
+++ day04/task16/my_str_isprintable.h
@@ -0,0 +1,6 @@
+#ifndef MY_STR_ISPRINTABLE
+#define MY_STR_ISPRINTABLE
+
+int my_str_isprintable(char const *str);
+
+#endif
